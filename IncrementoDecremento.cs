5133822b35af1b41465f2f92f610f2c67e32ab62
jtomaz99 <jtomaz99@yahoo.com.br>
2018-08-22 15:20:04 -0300

    Add files via upload

---
+++ IncrementoDecremento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncrementoDecremento : MonoBehaviour {
+
+
+    int carteira = 100;
+    int carteira2 = 200;
+    int carteira3 = 999;
+
+
+	void Start () {
+        //O ++ faz um incremento de 1 a mais como se fosse o +=1 do python
+        //pós incremento
+        print(carteira++);
+        print(carteira);
+
+        //pre incremento
+        print(++carteira2);
+
+        //decremento é com "--" e segue a regra anterior
+        carteira3 = (int)--carteira3;
+        print(--carteira3);
+
+
+
+    }
+
+
+    void Update () {
+		
+	}
+}
