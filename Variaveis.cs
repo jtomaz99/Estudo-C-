6535627086c53a34b8b7bef8dcb814aa4cfdd81d
jtomaz99 <jtomaz99@yahoo.com.br>
2018-08-22 12:33:00 -0300

    Add files via upload

---
+++ Variaveis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour
+{
+    /* tudo é comentario infinitos comentarios
+     oia
+     tudo tipo o """ do python
+     posso falar infinito aqui
+    */
+
+    //variaveis do game
+    int moedas = 10;
+    float anos = 2.34f;
+    bool duvida = false;
+    string texto = "Seu Zé";
+    // Use this for initialization
+	void Start () {
+        print (moedas);
+        print("idade : "); print(anos);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
