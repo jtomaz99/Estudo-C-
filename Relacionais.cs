670dddcb51c3d4163d4afbe2d02a6e9e53c400fd
jtomaz99 <jtomaz99@yahoo.com.br>
2018-08-23 00:48:27 -0300

    Add files via upload

---
+++ Relacionais.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Relacionais : MonoBehaviour {
+
+
+	/*
+	
+	
+	
+	
+	 */
+
+	int numero1 = 10;
+	int numero2 = 20;
+	// Use this for initialization
+	void Start () {
+		
+		//Operadores Lógicos.
+		print(numero1 == numero2 && numero1 > numero2); //false (and)
+		print(numero1 != numero2 && numero2 != numero1); //true (and)
+		print(numero1 > numero2 || numero2 > numero1); //true (or)
+		print(numero1 < numero2 || numero2 < numero1); //true (or)
+		print(numero1 >= numero2); //false
+		print(!(numero1 <= numero2)); //!(not)true == false
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
