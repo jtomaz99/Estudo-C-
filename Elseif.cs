e6f20ca428542f82ec8c310c0ea5849682e0bf62
José Tomáz <jtomaz99@yahoo.com.br>
2018-08-24 12:30:58 -0300

    Add files via upload

--- Elseif.cs
+++ Elseif.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Elseif : MonoBehaviour {
 
+	int moedas_heroi = 0;
+	int valor_vida = 100;
+	
 	// Use this for initialization
 	void Start () {
-		
+		if(moedas_heroi >= valor_vida){
+			moedas_heroi -= valor_vida;
+			print("você recebeu mais uma vida meu chapa.");
+		}
+		else if(moedas_heroi == 0){
+			print("Tentou me enganar? agora morra!!!");
+		}
+		else{
+			print("você não tem fundos suficientes!!! vá matar mais goblins.");
+		}
 	}
 	
 	// Update is called once per frame
